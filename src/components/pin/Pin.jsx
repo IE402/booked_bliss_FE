4ac3e610450a56aac9121280a7855ee1b93f3df7
hvupham <21522804@gm.uit.edu.vn>
2024-12-08 17:56:57 +0700

    add title map

--- src/components/pin/Pin.jsx
+++ src/components/pin/Pin.jsx
@@ -1,15 +1,21 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Link } from 'react-router-dom';
 import './pin.scss';
+import { useNavigate } from 'react-router-dom';
 
 function Pin({item}) {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/'+item.id)
+    }
     return (
         <Marker position={[item.latitude, item.longitude]}>
             <Popup>
                 <div className="popupContainer">
-                    <img src={item.images[0]} alt="" />
+                    <img onClick={handleClick} src={item.images[0]} alt="" />
                     <div className="textContainer">
-                        <Link to={`${item.id}`}>{item.title}</Link>
+                        <h3>{item.title}</h3>
+                        {/* <Link to={`${item.id}`}>{item.title}</Link> */}
                         <span>{item.bedroom} bedroom</span>
                         <b>$ {item.price}</b>
                     </div>
